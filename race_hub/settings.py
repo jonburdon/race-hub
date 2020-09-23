777a7ece31c7bad07dcdfaf00217eb12c68541ed
Jon Burdon <jon@branchoutwebsites.com>
2020-09-23 08:10:49 +0000

    Add sample css and placeholder nav

--- race_hub/settings.py
+++ race_hub/settings.py
@@ -148,6 +148,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproje  ct.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
