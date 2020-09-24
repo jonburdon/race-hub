2f8b2725de117981854892de44fbf0cdde758740
Jon Burdon <jon@branchoutwebsites.com>
2020-09-24 08:44:47 +0000

    Events render in events.html

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
+
+def all_events(request):
+    """ A view to show all events, including sorting and search queries """
+
+    events = Event.objects.all()
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, 'events/events.html', context)
