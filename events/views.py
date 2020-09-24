794363c01640eb138c265e4cf52c5c7042b9b8f5
Jon Burdon <jon@branchoutwebsites.com>
2020-09-24 10:32:23 +0000

    Search for events added

--- events/views.py
+++ events/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Event
 # Create your views here.
 
@@ -7,8 +9,22 @@ def all_events(request):
 
     events = Event.objects.all()
 
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('events'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            events = events.filter(queries)
+
+
     context = {
         'events': events,
+        'search_term': query,
     }
 
     return render(request, 'events/events.html', context)
