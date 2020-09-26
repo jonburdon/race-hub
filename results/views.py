1d885831a0c7f492ffd3a8f92c85b312e7703ea4
Jon Burdon <jon@branchoutwebsites.com>
2020-09-26 08:56:08 +0000

    Render results in simple paragraph and span elements

--- results/views.py
+++ results/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Result
 # Create your views here.
+
+def all_results(request):
+    """ A view to show all results, including sorting and search queries """
+
+    results = Result.objects.all()
+
+    context = {
+        'results': results,
+    }
+
+    return render(request, 'results/results.html', context)
