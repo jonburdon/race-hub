47da42deb4aedeb4d6ba1d0dcac4eb2b15ec3c2b
Jon Burdon <jon@branchoutwebsites.com>
2020-09-26 09:18:01 +0000

    Add search by athlete or age cat

--- results/views.py
+++ results/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Result
 # Create your views here.
 
@@ -6,9 +8,21 @@ def all_results(request):
     """ A view to show all results, including sorting and search queries """
 
     results = Result.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('results'))
+            
+            queries = Q(athlete__icontains=query) | Q(agecat__icontains=query)
+            results = results.filter(queries)
 
     context = {
         'results': results,
+        'search_term': query,
     }
 
     return render(request, 'results/results.html', context)
