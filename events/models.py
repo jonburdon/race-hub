174ce43f5c9630f9705b45bc4ffcb1b590440045
Jon Burdon <jon@branchoutwebsites.com>
2020-09-29 13:31:02 +0000

    Created checkout app and models

--- events/models.py
+++ events/models.py
@@ -47,6 +47,7 @@ class EventInstance(models.Model):
     eventdate = models.DateTimeField()
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     race_limit = models.DecimalField(max_digits=4, decimal_places=0, null=True, blank=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- race_hub/settings.py
+++ race_hub/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'results',
     'clubs',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
