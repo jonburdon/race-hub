bc8f19c914214b25114e6f343a88ff7c60dd544e
Jon Burdon <jon@branchoutwebsites.com>
2020-10-01 08:26:48 +0000

    Create profile app

--- race_hub/settings.py
+++ race_hub/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'clubs',
     'cart',
     'checkout',
+    'profiles',
 
     # Other apps
     'crispy_forms',
--- race_hub/urls.py
+++ race_hub/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('results/', include('results.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('my_racehub/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
