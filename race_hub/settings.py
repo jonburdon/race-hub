ea1a9d6665450a396071f2965e51870dd92132c5
Jon Burdon <jon@branchoutwebsites.com>
2021-03-19 10:55:47 +0000

    Fix indentation error in settings

--- race_hub/settings.py
+++ race_hub/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'race_hub.wsgi.application'
 
 
 
- if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
    }
