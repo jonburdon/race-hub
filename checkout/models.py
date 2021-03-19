0080bd0c7e45060066a0e5455eda2cef1030c668
Jon Burdon <jon@branchoutwebsites.com>
2021-03-19 19:35:31 +0000

    Order total fix in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -47,7 +47,7 @@ class Order(models.Model):
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
-        self.grand_total = self.order_total - self.delivery_cost - self.discount
+        self.grand_total = self.order_total + self.delivery_cost - self.discount
         self.save()
 
     def save(self, *args, **kwargs):
